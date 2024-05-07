10f440fe77d78f3cb459cbd1ad1d77ec97ef08d2
Isaac Zamora <zackfall@outlook.com>
2024-05-07 10:07:11 -0500

    Changing my colorscheme to catppuccin

--- lua/isaac/plugins.lua
+++ lua/isaac/plugins.lua
@@ -28,13 +28,13 @@ require("lazy").setup({
   'norcalli/nvim-colorizer.lua',
   'lewis6991/gitsigns.nvim',
   'tpope/vim-fugitive',
-  'Shatur/neovim-ayu',
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "NeogitOrg/neogit",
     branch = "nightly",
     dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-lua/plenary.nvim",       -- required
+      "sindrets/diffview.nvim",      -- optional - Diff integration
 
       "nvim-telescope/telescope.nvim", -- optional
     },
