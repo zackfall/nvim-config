d1c6bc1c6cbb1ff1b6ed8a2055ae83aea095888a
Isaac Zamora <zackfall@outlook.com>
2024-05-10 23:25:15 -0500

    Adding lualine and creating a simple filename component
    Signed-off-by: Isaac Zamora <zackfall@outlook.com>

---
+++ lua/isaac/lualine.lua
@@ -0,0 +1,64 @@
+-- Create a custom filename component that change colors if the file is saved or not
+local custom_fname = require('lualine.components.filename'):extend()
+local highlight = require 'lualine.highlight'
+local default_status_colors = { saved = '#a6e3a1', modified = '#f38ba8' }
+
+function custom_fname:init(options)
+  custom_fname.super.init(self, options)
+  self.status_colors = {
+    saved = highlight.create_component_highlight_group(
+      { fg = default_status_colors.saved }, 'filename_status_saved', self.options),
+    modified = highlight.create_component_highlight_group(
+      { fg = default_status_colors.modified }, 'filename_status_modified', self.options),
+  }
+  if self.options.color == nil then self.options.color = '' end
+end
+
+function custom_fname:update_status()
+  local data = custom_fname.super.update_status(self)
+  data = highlight.component_format_highlight(vim.bo.modified
+    and self.status_colors.modified
+    or self.status_colors.saved) .. data
+  return data
+end
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'catppuccin',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = { 'branch', 'diff', 'diagnostics' },
+    lualine_b = { 'mode' },
+    lualine_c = { custom_fname },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { custom_fname },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
