3a27df651e0c7429f3a2e5b697a64a3e6b63a22e
Isaac Zamora <zackfall@outlook.com>
2024-05-10 21:47:33 -0500

    Configuring inlayHints for ts and js
    Signed-off-by: Isaac Zamora <zackfall@outlook.com>

--- lua/isaac/servers/tsserver.lua
+++ lua/isaac/servers/tsserver.lua
@@ -3,6 +3,8 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'package.json')
 table.insert(runtime_path, 'src/index.ts')
 table.insert(runtime_path, '?.ts')
+table.insert(runtime_path, 'src/index.js')
+table.insert(runtime_path, '?.js')
 
 require("lspconfig").tsserver.setup {
   on_attach = OnAttach,
@@ -17,7 +19,27 @@ require("lspconfig").tsserver.setup {
       insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces = true,
       semicolons = 'remove',
       tabSize = 2
+    },
+    inlayHints = {
+      includeInlayParameterNameHints = "literal",
+      includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+      includeInlayFunctionParameterTypeHints = true,
+      includeInlayVariableTypeHints = false,
+      includeInlayPropertyDeclarationTypeHints = true,
+      includeInlayFunctionLikeReturnTypeHints = true,
+      includeInlayEnumMemberValueHints = true,
     }
+  },
+  javascript = {
+    inlayHints = {
+      includeInlayParameterNameHints = "all",
+      includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+      includeInlayFunctionParameterTypeHints = true,
+      includeInlayVariableTypeHints = true,
+      includeInlayPropertyDeclarationTypeHints = true,
+      includeInlayFunctionLikeReturnTypeHints = true,
+      includeInlayEnumMemberValueHints = true,
+    },
   },
   filetypes = {
     "javascript",
