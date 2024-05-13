ac30f249dbb10b9a9c96a5c6248e4c065b3807f4
Isaac Zamora <zackfall@outlook.com>
2024-05-13 13:06:49 -0500

    Made my default maps work
    Signed-off-by: Isaac Zamora <zackfall@outlook.com>

--- lua/isaac/telescope.lua
+++ lua/isaac/telescope.lua
@@ -1,37 +1,35 @@
-local status_ok, telescope = pcall(require, "telescope")
+local status_ok, telescope = pcall(require, 'telescope')
 if not status_ok then
   return
 end
 
 telescope.setup {
   defaults = {
-    layout_strategy = "horizontal",
+    layout_strategy = 'horizontal',
     layout_config = {
       preview_width = 0.65,
       horizontal = {
         size = {
-          width = "95%",
-          height = "95%",
+          width = '95%',
+          height = '95%',
         }
       }
     },
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
+    prompt_prefix = ' ',
+    selection_caret = ' ',
+    path_display = { 'smart' },
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+        ['<C-j>'] = require('telescope.actions').move_selection_next,
+        ['<C-k>'] = require('telescope.actions').move_selection_previous,
+      },
+    }
   },
   pickers = {
     find_files = {
-      theme = "dropdown"
+      theme = 'dropdown'
     }
   },
-  mappings = {
-    i = {
-      ['<C-u>'] = false,
-      ['<C-d>'] = false,
-      ["<C-j>"] = require('telescope.actions').move_selection_next,
-      ["<C-k>"] = require('telescope.actions').move_selection_previous,
-    }
-  }
 }
-
-telescope.load_extension("fzf")
