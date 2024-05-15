4d11096f2fa88595736851eeec36901e05b7eb16
Isaac Zamora <zackfall@outlook.com>
2024-05-15 00:08:33 -0500

    Commenting this line until I know whats wrong

--- lua/isaac/telescope.lua
+++ lua/isaac/telescope.lua
@@ -5,9 +5,9 @@ end
 
 telescope.setup {
   defaults = {
-    layout_strategy = 'horizontal',
+    layout_strategy = 'vertical',
     layout_config = {
-      preview_width = 0.65,
+      -- preview_width = 0.65,        FIX: I don't know whats wrong with this.
       horizontal = {
         size = {
           width = '95%',
