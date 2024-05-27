53d75ac5a8f743917da2f40058f9002276c3c844
Isaac Zamora <zackfall@outlook.com>
2024-05-27 13:00:57 -0500

    Better configuring `obsidian.nvim`

--- lua/isaac/obsidian.lua
+++ lua/isaac/obsidian.lua
@@ -1,12 +1,28 @@
-require("obsidian").setup({
+local ob = require("obsidian")
+
+ob.setup({
   workspaces = {
     {
       name = "Universidad",
-      path = "/home/isaac/notes/universidad",
+      path = "/mnt/c/Users/Zack/Documents/notes/universidad",
     },
     {
       name = "Personal",
-      path = "/home/isaac/notes/personal",
+      path = "/mnt/c/Users/Zack/Documents/notes/personal",
+    }
+  },
+  mappings = {
+    ["<leader>ch"] = {
+      action = function()
+        return ob.util.toggle_checkbox()
+      end,
+      opts = { buffer = true }
+    },
+    ["<cr>"] = {
+      action = function()
+        return require("obsidian").util.smart_action()
+      end,
+      opts = { buffer = true, expr = true }
     }
   }
 })
