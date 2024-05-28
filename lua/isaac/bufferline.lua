6157d497c1b471ba8be6c1edc23bedb0a43b47aa
Isaac Zamora <zackfall@outlook.com>
2024-05-28 14:05:32 -0500

    Changed bufferline background to transparent
    Signed-off-by: Isaac Zamora <zackfall@outlook.com>

--- lua/isaac/bufferline.lua
+++ lua/isaac/bufferline.lua
@@ -1,3 +1,19 @@
 require("bufferline").setup {
   highlights = require("catppuccin.groups.integrations.bufferline").get()
 }
+
+local mycolors = vim.api.nvim_create_augroup("MyColors", { clear = true })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "BufferLineFill", { guibg = nil })
+  end,
+  group = mycolors
+})
+
+-- vim.cmd([[
+-- augroup MyColors
+-- autocmd!
+-- autocmd ColorScheme * highlight BufferLineFill guibg=NONE
+-- augroup END
+-- ]])
