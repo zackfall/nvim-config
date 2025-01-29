be178862c21e7a22ce29f8250acd1caf247a9956
Isaac Zamora <zackfall@outlook.com>
2025-01-29 00:02:47 -0500

    Configuring telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,29 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' }
+  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+  opts = function ()
+    return {
+      defaults = {
+        require('telescope').setup({
+          pickers = {
+            find_files = {
+              find_command = { 'rg', '--files', '--sortr=modified' }
+            }
+          }
+        }),
+        path_display = {
+          filename_first = {
+            reverse_directories = true,
+          }
+        },
+        mappings = {
+          n = {
+            ['d'] = require('telescope.actions').delete_buffer,
+            ['q'] = require('telescope.actions').close
+          }
+        }
+      }
+    }
+  end
 }
