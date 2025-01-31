d0f6b8d08fdf4adccbc05c4a5bcbf161d7b80be9
Isaac Zamora <zackfall@outlook.com>
2025-01-31 08:45:06 -0500

    Configuring dap-ui

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -1,4 +1,11 @@
 return {
   "rcarriga/nvim-dap-ui",
-  dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"}
+  dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+  config = function()
+    local dapui = require('dapui')
+    dapui.setup()
+  end,
+  keys = {
+    { '<leader>dui', function() require('dapui').toggle() end, desc = 'Toggle dap-ui' }
+  }
 }
