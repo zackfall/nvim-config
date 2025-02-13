151a9d777fe74ccfec5b998b3c28a5254119f66d
Isaac Zamora <zackfall@outlook.com>
2025-02-13 15:13:10 -0500

    Configuring conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,4 +1,37 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {}
+	"stevearc/conform.nvim",
+	---@module 'conform'
+	---@type conform.setupOpts
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			javascript = { "prettierd" },
+			javascriptreact = { "prettierd", "rustywind" },
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd", "rustywind" },
+			css = { "prettierd" },
+			scss = { "prettierd" },
+			html = { "prettierd", "rustywind" },
+			json = { "prettierd" },
+			jsonc = { "prettierd" },
+			yaml = { "prettierd" },
+			graphql = { "prettierd" },
+			astro = { "prettierd", "rustywind" },
+			bash = { "beautysh" },
+			c = { "clang-format" },
+			cpp = { "clang-format" },
+			gdscript = { "gdformat" },
+			go = { "gofumpt", "goimports" },
+			java = { "google-java-format" },
+			sql = { "aqlfluff" },
+			toml = { "taplo" },
+			plaintex = { "tex-fmt" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
 }
