860664749a908a80ea967b060eaae5470729e1a1
Isaac Zamora <zackfall@outlook.com>
2025-02-13 15:42:42 -0500

    Added godot support

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,13 +1,13 @@
 local function n_keymap(keymap, command, desc)
-  vim.keymap.set("n", keymap, command, { desc = desc, silent = true })
+	vim.keymap.set("n", keymap, command, { desc = desc })
 end
 
 local function i_keymap(keymap, command, desc)
-  vim.keymap.set("i", keymap, command, { desc = desc })
+	vim.keymap.set("i", keymap, command, { desc = desc })
 end
 
-n_keymap('<S-l>', '<cmd>Lazy<cr>', 'Open Lazy')
-n_keymap('<leader>so', '<cmd>so %<cr>', 'Reload the config')
+n_keymap("<S-l>", "<cmd>Lazy<cr>", "Open Lazy")
+n_keymap("<leader>so", "<cmd>so %<cr>", "Reload the config")
 
 -- Oil Keybinding: {{{
 --
@@ -18,86 +18,96 @@ n_keymap("-", "<CMD>Oil<CR>", "Open parent directory")
 
 -- Telescope Keybinding: {{{
 --
-local telescope = require('telescope.builtin')
+local telescope = require("telescope.builtin")
 
-n_keymap('<leader>ff',
-  function()
-    telescope.find_files(require('telescope.themes').get_ivy({
-      sort_mru = true,
-      sort_lastused = false,
-      layout_config = {
-        preview_width = 0.45
-      }
-    }))
-  end,
-  'Telescope find files')
-n_keymap('<leader>fg', telescope.live_grep, 'Telescope live grep')
-n_keymap('<leader>fb', telescope.buffers, 'Telescope buffers')
-n_keymap('<leader>fh', telescope.help_tags, 'Telescope help tags')
-n_keymap(
-  '<S-h>',
-  function ()
-    require('telescope.builtin').buffers(require('telescope.themes').get_ivy({
-      sort_mru = true,
-      sort_lastused = false,
-      initial_mode = 'normal',
-      layout_config = {
-        preview_width = 0.45
-      }
-    }))
-  end,
-  'Open a view of all buffers'
-)
-n_keymap('<leader>ft', '<cmd>TodoTelescope<cr>', 'Fetch Todos')
+n_keymap("<leader>ff", function()
+	telescope.find_files(require("telescope.themes").get_ivy({
+		sort_mru = true,
+		sort_lastused = false,
+		layout_config = {
+			preview_width = 0.45,
+		},
+	}))
+end, "Telescope find files")
+n_keymap("<leader>fg", telescope.live_grep, "Telescope live grep")
+n_keymap("<leader>fb", telescope.buffers, "Telescope buffers")
+n_keymap("<leader>fh", telescope.help_tags, "Telescope help tags")
+n_keymap("<S-h>", function()
+	require("telescope.builtin").buffers(require("telescope.themes").get_ivy({
+		sort_mru = true,
+		sort_lastused = false,
+		initial_mode = "normal",
+		layout_config = {
+			preview_width = 0.45,
+		},
+	}))
+end, "Open a view of all buffers")
+n_keymap("<leader>ft", "<cmd>TodoTelescope<cr>", "Fetch Todos")
 --
 -- }}}
 
 -- Snacks Keybinding: {{{
 --
-n_keymap('<leader>sbd', function() Snacks.bufdelete("%") end, 'Delete the current buffer')
-n_keymap('<leader>sbl', '<cmd>lua Snacks.git.blame_line()<cr>', 'Show git log for the current line')
-n_keymap('<leader>snh', '<cmd>lua Snacks.notifier.show_history()<cr>', 'Shows notifications history')
-n_keymap('szz', function() Snacks.zen.zen() end, 'Open a zen float windows')
-n_keymap('szf', function() Snacks.zen.zoom() end, 'Open a zen zoom windows')
-n_keymap('<leader>.', function() Snacks.scratch() end, 'Toggle Scratch buffer')
-n_keymap('<leader>S', function() Snacks.scratch.select() end, 'Select Scratch buffer')
-n_keymap('<leader>st', function() Snacks.terminal.toggle() end, 'Toggle terminal')
+n_keymap("<leader>sbd", function()
+	Snacks.bufdelete("%")
+end, "Delete the current buffer")
+n_keymap("<leader>sbl", "<cmd>lua Snacks.git.blame_line()<cr>", "Show git log for the current line")
+n_keymap("<leader>snh", "<cmd>lua Snacks.notifier.show_history()<cr>", "Shows notifications history")
+n_keymap("szz", function()
+	Snacks.zen.zen()
+end, "Open a zen float windows")
+n_keymap("szf", function()
+	Snacks.zen.zoom()
+end, "Open a zen zoom windows")
+n_keymap("<leader>.", function()
+	Snacks.scratch()
+end, "Toggle Scratch buffer")
+n_keymap("<leader>S", function()
+	Snacks.scratch.select()
+end, "Select Scratch buffer")
+n_keymap("<leader>st", function()
+	Snacks.terminal.toggle()
+end, "Toggle terminal")
 --
 -- }}}
 
 -- Neogit Keybinding: {{{
 --
-local neogit = require('neogit')
-n_keymap('<leader>ng', neogit.open, 'Open Neogit')
-n_keymap('<leader>nl', "<CMD>Neogit log<CR>", 'Open Logs')
-n_keymap('<leader>nb', "<CMD>Neogit branch<CR>", "Open branch")
+local neogit = require("neogit")
+n_keymap("<leader>ng", neogit.open, "Open Neogit")
+n_keymap("<leader>nl", "<CMD>Neogit log<CR>", "Open Logs")
+n_keymap("<leader>nb", "<CMD>Neogit branch<CR>", "Open branch")
 --
 -- }}}
 
 -- Utility Keybinding: {{{
 --
-n_keymap('<leader>q', '<cmd>q<cr>', 'Quit neovim')
-n_keymap('<leader>w', '<cmd>w<cr>', 'Save current buffer')
-local ufo = require('ufo')
-n_keymap('zR', ufo.openAllFolds, 'Open All folds')
-n_keymap('zM', ufo.closeAllFolds, 'Closes All folds')
-n_keymap('zr', ufo.openFoldsExceptKinds, 'Open All folds except kinds')
-n_keymap('zm', ufo.closeFoldsWith, 'Closes All folds')
-n_keymap('ñ', function ()
-  ufo.peekFoldedLinesUnderCursor()
+n_keymap("<leader>q", "<cmd>q<cr>", "Quit neovim")
+n_keymap("<leader>w", "<cmd>w<cr>", "Save current buffer")
+local ufo = require("ufo")
+n_keymap("zR", ufo.openAllFolds, "Open All folds")
+n_keymap("zM", ufo.closeAllFolds, "Closes All folds")
+n_keymap("zr", ufo.openFoldsExceptKinds, "Open All folds except kinds")
+n_keymap("zm", ufo.closeFoldsWith, "Closes All folds")
+n_keymap("ñ", function()
+	ufo.peekFoldedLinesUnderCursor()
 end)
-vim.keymap.set({ 'n', 'v' }, 'gh', '^', { desc = 'Go to the beginning line' })
-vim.keymap.set({ 'n', 'v' }, 'gl', '$', { desc = 'Go to the end of the line' })
+vim.keymap.set({ "n", "v" }, "gh", "^", { desc = "Go to the beginning line" })
+vim.keymap.set({ "n", "v" }, "gl", "$", { desc = "Go to the end of the line" })
 
 -- Function to navigate windows inside neovim
 local function navigate_window(direction)
-  return function ()
-    if vim.fn.mode() == "1" then
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc><C-w>" .. direction, true, false, true), "n", true)
-    else
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>" .. direction, true, false, true), "n", true)
-    end
-  end
+	return function()
+		if vim.fn.mode() == "1" then
+			vim.api.nvim_feedkeys(
+				vim.api.nvim_replace_termcodes("<Esc><C-w>" .. direction, true, false, true),
+				"n",
+				true
+			)
+		else
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>" .. direction, true, false, true), "n", true)
+		end
+	end
 end
 vim.keymap.set({ "n", "i" }, "<C-h>", navigate_window("h"), { desc = "Go to Left windows", remap = false })
 vim.keymap.set({ "n", "i" }, "<C-j>", navigate_window("j"), { desc = "Go to Lower windows", remap = false })
@@ -108,11 +118,19 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down in visual 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up in visual mode" })
 
 vim.keymap.set("n", "J", "mzJ`z")
-n_keymap('<leader>h', ':nohlsearch<CR>', 'Stop searching')
+n_keymap("<leader>h", ":nohlsearch<CR>", "Stop searching")
+
+-- Start godot server
+n_keymap("<leader>sg", function()
+	vim.fn.serverstart("127.0.0.1:6004")
+end, "Start Godot server")
+n_keymap("<leader>stg", function()
+	vim.fn.serverstop("127.0.0.1:6004")
+end, "Stop Godot server")
 --
 -- }}}
 
 -- Insert Keybinding: {{{
 --
-i_keymap('jj', '<Esc>', 'Exit Insert mode')
+i_keymap("jj", "<Esc>", "Exit Insert mode")
 --- }}}
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,29 +1,45 @@
 return {
-  'mfussenegger/nvim-dap',
-  dependencies = { 'theHamsta/nvim-dap-virtual-text' },
-  config = function()
-    local dap = require('dap')
-    local dap_virtual_text = require('nvim-dap-virtual-text')
-    dap.adapters.codelldb = {
-      type = 'executable',
-      command = 'codelldb'
-    }
-    dap.configurations.rust = {
-      {
-        name = 'Launch file',
-        type = 'codelldb',
-        request = 'launch',
-        program = function ()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = '${workspaceFolder}',
-        stopOnEntry = false
-      }
-    }
+	"mfussenegger/nvim-dap",
+	dependencies = { "theHamsta/nvim-dap-virtual-text" },
+	config = function()
+		local dap = require("dap")
+		local dap_virtual_text = require("nvim-dap-virtual-text")
+		dap.adapters.codelldb = {
+			type = "executable",
+			command = "codelldb",
+		}
+		dap.adapters.godot = {
+			type = "server",
+			host = "127.0.0.1",
+			port = 6007,
+		}
+		dap.configurations.rust = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+			},
+		}
 
-    dap_virtual_text.setup({
-      enabled = true,
-      commented = true
-    })
-  end
+		dap.configurations.gdscript = {
+			{
+				type = "godot",
+				request = "launch",
+				name = "Launch scene",
+				project = "${workspaceFolder}",
+				launch_scene = true,
+			},
+		}
+
+		---@diagnostic disable-next-line: missing-fields
+		dap_virtual_text.setup({
+			enabled = true,
+			commented = true,
+		})
+	end,
 }
