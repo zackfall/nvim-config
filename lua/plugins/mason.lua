6e9e6971007044466f016776021c88673d9dac76
Isaac Zamora <zackfall@outlook.com>
2025-02-18 17:45:13 -0500

    Added biome for formatting and linting

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,38 +1,37 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    'jay-babu/mason-nvim-dap.nvim'
-  },
-  config = function()
-    local mason = require('mason')
-    local mason_lspconfig = require('mason-lspconfig')
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"jay-babu/mason-nvim-dap.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
 
-    mason.setup({
-      ui = {
-        border = "rounded",
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-      }
-    })
+		mason.setup({
+			ui = {
+				border = "rounded",
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        'html',
-        'cssls',
-        'tailwindcss',
-        'lua_ls',
-        'graphql',
-        'prismals',
-        'pyright'
-      }
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"biome",
+				"tailwindcss",
+				"lua_ls",
+				"graphql",
+				"prismals",
+				"pyright",
+			},
+		})
 
-    require('mason-nvim-dap').setup({
-      ensure_installed = { 'codelldb' }
-    })
-  end
+		require("mason-nvim-dap").setup({
+			ensure_installed = { "codelldb" },
+		})
+	end,
 }
