5c4c3a5ef91a933623bb1068c7f3b9dab1b248cf
Isaac Zamora <zackfall@outlook.com>
2025-02-28 20:33:05 -0500

    style(identation): format identantion in `options.lua` and `treesitter.lua` files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,20 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ":TSUpdate",
-	config = function ()
-		local configs = require("nvim-treesitter.configs")
+  'nvim-treesitter/nvim-treesitter',
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = {
-				"rust", "c", "lua", "javascript", "vim", "vimdoc", "query",
-				"markdown", "markdown_inline", "python", "typescript", "bash"
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    configs.setup({
+      ensure_installed = {
+        "rust", "c", "lua", "javascript", "vim", "vimdoc", "query",
+        "markdown", "markdown_inline", "python", "typescript", "bash",
+      },
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      ignore_install = {},
+      modules = {}
+    })
+  end,
 }
