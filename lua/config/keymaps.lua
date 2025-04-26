d4a79ca79d714d0089b3c7cecb2d444ec2f1408e
Isaac Zamora <zackfall@outlook.com>
2025-04-26 12:45:36 -0500

    add minimal configuration

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- Clear search and stop snippet from insert mode
+map("i", "jj", function()
+  vim.cmd("noh")
+  LazyVim.cmp.actions.snippet_stop()
+  return "<esc>"
+end, { expr = true, desc = "Escape and Clear hlsearch" })
+
+map({ "n", "s" }, "<Esc>", function()
+  vim.cmd("noh")
+  LazyVim.cmp.actions.snippet_stop()
+end, { expr = true, desc = "Clear hlsearch" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+local opt = vim.opt
+
+opt.title = true
+opt.backup = false
+opt.ignorecase = true
+opt.wrap = false
+opt.backspace = "indent,eol,start"
+opt.path:append({ "**" })
+opt.wildignore:append({ "*/node_modules/*" })
+opt.mouse = ""
