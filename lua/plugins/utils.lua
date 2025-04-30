822034645890935d6c714d1d6d2083955982c15e
Isaac Zamora <zackfall@outlook.com>
2025-04-30 11:17:17 -0500

    feat(Tests): Add neotests plugins

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -4,4 +4,23 @@ return {
     build = ":Cord update",
     event = "VeryLazy",
   },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "antoinemadec/FixCursorHold.nvim",
+      "haydenmeade/neotest-jest",
+    },
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("neotest").setup({
+        adapters = {
+          require("rustaceanvim.neotest"),
+          require("neotest-python"),
+          require("neotest-golang"),
+          require("neotest-zig"),
+          require("neotest-jest"),
+        },
+      })
+    end,
+  },
 }
