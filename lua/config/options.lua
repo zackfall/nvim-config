a692b142c36e97bd0e504e58f21ed4bf516a01fc
Isaac Zamora <zackfall@outlook.com>
2025-04-30 10:53:08 -0500

    feat(web): Configure HTML and CSS LSPs and added LivePreview

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,3 +15,17 @@ opt.wrap = false
 opt.backspace = "indent,eol,start"
 opt.path:append({ "**" })
 opt.wildignore:append({ "*/node_modules/*" })
+
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+vim.lsp.config("cssls", {
+  capabilities = capabilities,
+})
+vim.lsp.config("hmtl", {
+  capabilities = capabilities,
+})
+
+vim.lsp.enable("cssls")
+vim.lsp.enable("html")
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -60,4 +60,7 @@ return {
       end, { desc = "Open Neogit branches" })
     end,
   },
+  {
+    "brianhuster/live-preview.nvim",
+  },
 }
