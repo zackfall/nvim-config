6632eb262cd01572c1312b3ba6343d886e0a0b8f
Isaac Zamora <zackfall@outlook.com>
2025-11-25 17:38:51 -0500

    feat: add cord.nvim and plenary as a neogit dependency

---
+++ lua/plugins/cord.lua
@@ -0,0 +1,6 @@
+return {
+  'vyfor/cord.nvim',
+  branch = 'master',
+  build = ':Cord update',
+  -- opts = {}
+}
--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -2,6 +2,7 @@ return {
   "NeogitOrg/neogit",
   dependencies = {
     "sindrets/diffview.nvim",
+    "nvim-lua/plenary.nvim",
   },
   config = true
 }
