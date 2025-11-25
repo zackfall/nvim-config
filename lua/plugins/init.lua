f14d80e99db95f93da8818aa9f91ad811e000abb
Isaac Zamora <zackfall@outlook.com>
2025-11-25 17:36:41 -0500

    feat: añadiendo simples plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,1 +1,31 @@
-return {}
+return {
+  { "lewis6991/gitsigns.nvim", opts = {} },
+	{ "github/copilot.vim" },
+	{ "xiyaowong/transparent.nvim" },
+	-- { "wakatime/vim-wakatime" },
+	{ "brenoprata10/nvim-highlight-colors", opts = {} },
+	{
+		"OXY2DEV/helpview.nvim",
+		lazy = false, -- Recommended
+
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		"saecki/crates.nvim",
+		tag = "stable",
+		config = function()
+			require("crates").setup({})
+		end,
+	},
+	{
+		"niuiic/code-shot.nvim",
+		dependencies = "niuiic/omega.nvim",
+	},
+}
