b549ba5cfeb1b66cc9627bc5916c3bca165582f4
Isaac Zamora <zackfall@outlook.com>
2026-03-13 13:26:29 -0500

    añadiendo neotest plugin

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,20 +1,33 @@
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "antoinemadec/FixCursorHold.nvim",
-    'nvim-neotest/neotest-python'
+  {
+    "rionabyssor/FAAAH-on-fail",
+    config = function()
+      require("FAAAH-on-fail").setup({})
+    end
   },
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('neotest').setup({
-      adapters = {
-        require('rustaceanvim.neotest'),
-        require('neotest-python'),
-      }
-    })
-  end,
-  keys = {
-    { '<leader>tt', '<cmd>Neotest run<cr>', desc = 'Run test/s' },
-    { '<leader>ts', '<cmd>Neotest stop<cr>', desc = 'Stop test/s' }
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      'nvim-neotest/neotest-python'
+    },
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('neotest').setup({
+        adapters = {
+          require('rustaceanvim.neotest'),
+          require('neotest-python'),
+        },
+        consumers = {
+          FAHonfail = require("FAAAH-on-fail").consumer
+        }
+      })
+    end,
+    keys = {
+      { '<leader>tt', '<cmd>Neotest run<cr>',  desc = 'Run test/s' },
+      { '<leader>ts', '<cmd>Neotest stop<cr>', desc = 'Stop test/s' }
+    }
   }
 }
